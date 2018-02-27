0fd5760bd6a02903312949aceb59b711028734ba
suiningys <suiningys@126.com>
2018-02-27 21:43:22 +0800

    read spec data as DataFrame

--- readData.py
+++ readData.py
@@ -5,8 +5,18 @@ __author__ = "Sol Young"
 __mtime__ = "2018/2/26"
 bug fuck off!!!
 """
+import numpy as np
 import pandas as pd
+from pandas import Series, DataFrame
+
+def readData():
+    dataPath = './COCO2CH4.xlsx'
+    d = pd.read_excel(dataPath)
+    CO = d['CO']
+    CH4 = d['CH4']
+    C02 = d['CO2']
+    specData = d.iloc[:,5:]
+
 
 if __name__=="__main__":
-    dataPath = './gasspec.xls'
-    d = pd.read_excel(dataPath)
+    pass
