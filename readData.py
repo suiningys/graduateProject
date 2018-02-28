7374466e3741f320073a21744ea5273f30cb27cb
suiningys <suiningys@126.com>
2018-02-28 14:54:20 +0800

    some linear model

--- readData.py
+++ readData.py
@@ -9,14 +9,51 @@ import numpy as np
 import pandas as pd
 from pandas import Series, DataFrame
 
+from sklearn import linear_model
+from sklearn import cross_decomposition
+from sklearn import model_selection
+
 def readData():
     dataPath = './COCO2CH4.xlsx'
     d = pd.read_excel(dataPath)
     CO = d['CO']
     CH4 = d['CH4']
-    C02 = d['CO2']
+    CO2 = d['CO2']
     specData = d.iloc[:,5:]
+    return CO,CO2,CH4,specData
 
+def PLS(xTest, yTest, xTrain, yTrain, nComponents):
+    plsModel = cross_decomposition.PLSRegression(n_components=nComponents)
+    plsModel.fit(xTrain,yTrain)
+    coef = plsModel.coef_
+    yPredict = plsModel.predict(xTest)
+    return yPredict, coef
+
+def useRidgeRegression(xTest, yTest, xTrain, yTrain):
+    ridgeModel = linear_model.Ridge(alpha=0.01, fit_intercept=True,max_iter=10000)
+    ridgeModel.fit(xTrain,yTrain)
+    ceof = ridgeModel.coef_
+    yPredict = ridgeModel.predict(xTest)
+    return yPredict, ceof
+
+def useLasso(xTest, yTest, xTrain, yTrain):
+    lassoModel = linear_model.Lasso(alpha=1.0, fit_intercept=True, max_iter=10000)
+    lassoModel.fit(xTrain,yTrain)
+    ceof = lassoModel.coef_
+    yPredict = lassoModel.predict(xTest)
+    return yPredict, ceof
+
+def useElasticNet(xTest, yTest, xTrain, yTrain):
+    enModel = linear_model.ElasticNet(l1_ratio=0.7)
+    enModel.set_params(alpha=0.1)
+    enModel.fit(xTrain,yTrain)
+    coef = enModel.coef_
+    yPredict = enModel.predict(xTest)
+    return yPredict,coef
 
 if __name__=="__main__":
-    pass
+    CO, CO2, CH4, specData = readData()
+    xData = specData
+    yData = CO
+    xTrain, xTest, yTrain, yTest = \
+        model_selection.train_test_split(xData,yData,test_size=0.25, random_state=42)
