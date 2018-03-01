ca184a08800936e528e4b1dfe0c5a86fb3fbca90
suiningys <suiningys@126.com>
2018-03-01 22:34:55 +0800

    add part of GA, including mutation and crossover

--- heuristic_algorithm.py
+++ heuristic_algorithm.py
@@ -5,3 +5,68 @@ __author__ = "Sol Young"
 __mtime__ = "2018/3/1"
 bug fuck off!!!
 """
+import numpy as np
+import pandas as pd
+from pandas import Series, DataFrame
+
+from sklearn import linear_model
+from sklearn import cross_decomposition
+from sklearn import model_selection
+from sklearn import metrics as sm
+
+from readData import readData
+
+class individual(object):
+    def __init__(self,chromo = None,fitness = None,lv = None):
+        self.chromo = chromo
+        self.fitness = fitness
+        self.lv = lv
+
+class GeneAlgorithm(object):
+    def __init__(self,xTrain,yTrain,
+                 idv = 10, crossProb = 0.6, mutationProb = 0.01,mutBits = 1,
+                 Chromo = None, maxIter = 100):
+        trans, features = xTrain.shape
+        self.xData = xTrain
+        self.yData = yTrain
+        self.idv = idv#个体个数
+        self.crossProb = crossProb#交叉概率
+        self.mutationProb = mutationProb#变异概率
+        self.chromeBits = features#基因位点
+        self.mutBits = mutBits#每次变异的位数
+        if Chromo is None:#可以接受外部传入的初始化基因
+            self.Chromo = np.random.randint(0,2,size = [self.idv,self.chromeBits])#初始化基因
+        else:
+            self.Chromo = Chromo
+        #全局最优变量
+        self.globalBestFitness = np.inf
+        self.globalBestChromo = np.random.random([1,self.chromeBits])
+        self.globalBestLv = features
+
+        #构造种群
+        self.population = []
+        for ii in range(self.idv):
+            self.population.append(individual())
+        #初始化种群
+        for indiv, ii in zip(self.population,list(range(self.idv))):
+            indiv.chromo = self.Chromo[ii,:]
+            indiv.fitness = np.inf
+            indiv.lv = indiv.chromo.shape[0]
+        #局部最优
+        self.currentBestFitness = np.inf
+        self.currentBestChromo = None
+
+        self.maxIter = maxIter#最大迭代次数
+
+    def mutation(self,Chromo):
+        mutProp = np.random.random()
+        if mutProp<self.mutationProb:
+            mutBitPos = np.random.randint(0,self.chromeBits)
+            Chromo[mutBitPos] ^= 1#取反
+
+    def crossOver(self,Chrome1,Chrome2):
+        crossBitPos = np.random.randint(0,self.chromeBits)
+        Chrome1Tail = Chrome1[crossBitPos:].copy()
+        Chrome2Tail = Chrome2[crossBitPos:].copy()
+        Chrome1[crossBitPos:] = Chrome2Tail
+        Chrome2[crossBitPos:] = Chrome1Tail
--- unitTest.py
+++ unitTest.py
@@ -16,6 +16,9 @@ from readData import UVECV
 from readData import UVE
 from readData import PLSwithAllFeatures
 
+from heuristic_algorithm import individual
+from heuristic_algorithm import GeneAlgorithm
+
 class testCV(unittest.TestCase):
     def test_init(self):
         pass
@@ -46,6 +49,22 @@ class testCV(unittest.TestCase):
         yPredict, R2, MSE, R2P = PLSwithAllFeatures(xTest, yTest, xTrain, yTrain)
         print("R2P is", R2P)
         self.assertIsInstance(yPredict, np.ndarray)
+
+class testGA(unittest.TestCase):
+
+    def testCO(self):
+        CO, CO2, CH4, specData = readData()
+        xTrain, xTest, yTrain, yTest = \
+            train_test_split(specData, CO, test_size=0.25, random_state=42)
+        GACO = GeneAlgorithm(xTrain,yTrain)
+        # ind1 = individual(chrome=np.random.randint(0,1,size=[1,10]))
+        # ind2 = individual(chrome=np.random.randint(0, 1, size=[1, 10]))
+        GACO.population[0].chromo = np.ones(GACO.chromeBits)
+        GACO.population[1].chromo = np.zeros(GACO.chromeBits)
+        GACO.crossOver(GACO.population[0].chromo,GACO.population[1].chromo)
+        print(np.sum(GACO.population[0].chromo))
+        print(np.sum(GACO.population[1].chromo))
+        #GACO.crossOver(np.ones([1,GACO.chromeBits]), np.zeros([1,GACO.chromeBits]))
 
 if __name__=="__main__":
     unittest.main()
