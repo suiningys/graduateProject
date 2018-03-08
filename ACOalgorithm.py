5d0a5aae5fa7713a5bd70f636808ad9777158a4b
suiningys <suiningys@126.com>
2018-03-08 21:22:43 +0800

    change PSO to ACO

---
+++ ACOalgorithm.py
@@ -0,0 +1,18 @@
+# -*- coding: utf-8 -*-
+"""
+__title__ = ""
+__author__ = "Sol Young"
+__mtime__ = "2018/3/8"
+bug fuck off!!!
+"""
+
+import numpy as np
+
+from readData import plsRegressAnalysis
+from geneticAlgorithm import individual
+
+class acoAlgorithm(object):
+    def __init__(self, xTrain, yTrain,idv=10,
+                 Chromo=None, maxIter=100,
+                 fitnessFunction=plsRegressAnalysis):
+        pass
